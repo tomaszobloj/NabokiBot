d43cc3c43f136a7e833d7402fad29a61c9090993
Tomasz Obłój <61419484+tomaszobloj@users.noreply.github.com>
2021-08-15 14:59:19 +0000

    witam

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
 import discord
 import os
+import random
 from keep_alive import keep_alive
 
 client = discord.Client()
+witam_list = ["Witam!","WITAM!","witam!","Witam","WITAM","witam"]
 
 #bot loggin
 @client.event
 async def on_ready():
     print("Logged in as {0.user}".format(client))
 
-#bot saying witam
+#bot messages
 @client.event
 async def on_message(message):
     if message.author == client.user:
-      return
+        return
 
     if message.content.startswith('WITAM'):
-      await message.channel.send('WITAM!')
+        await message.channel.send(random.choice(witam_list))
 
 #web server
 keep_alive()
+
 #bot run
 client.run(os.getenv('TOKEN'))
