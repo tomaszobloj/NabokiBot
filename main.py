9c94a1b8bf910a6d5d6d09295bfdb5c8cafbdd65
Tomasz Obłój <61419484+tomaszobloj@users.noreply.github.com>
2021-09-19 10:00:47 +0000

    WITAM command

--- main.py
+++ main.py
@@ -11,6 +11,21 @@ client = commands.Bot(command_prefix = "~")
 async def on_ready():
     print("Logged in as {0.user}".format(client))
 
+#member joined a server
+@client.event
+async def on_member_join(member):
+  print(f'{member} dołączył.')
+
+#member left a server
+@client.event
+async def on_member_remove(member):
+  print(f'{member} odszedł.')
+
+#bot commands
+@client.command()
+async def WITAM(ctx):
+  await ctx.send('WITAM')
+
 #web server
 keep_alive()
 
