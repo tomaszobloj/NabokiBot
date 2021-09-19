1bba33d84e7ecb2c05e0ddf075228134ba33df3d
Tomasz Obłój <61419484+tomaszobloj@users.noreply.github.com>
2021-09-19 09:46:26 +0000

    bot rewrite

--- main.py
+++ main.py
@@ -2,49 +2,14 @@ import discord
 import os
 import random
 from keep_alive import keep_alive
+from discord.ext import commands
 
-client = discord.Client()
-
-help_list = """:pig: Lista komend bota NABOKI: 
-:arrow_forward: HELP - komenda pomocy
-:arrow_forward: WITAM - przywitanie się z NABOKI, który odpowiada pozdowieniem z listy słów
-:arrow_forward: KRYCHA - komenda wyłączona"""
-
-witam_list = ["WITAM!",
-              "Witam!",
-              "witam!",
-              "WITAM",
-              "Witam",
-              "witam",
-              "WITAM...",
-              "Witam...",
-              "witam...",
-              "Kij Ci w oko",
-              "SIEMA",
-              "Siema",
-              "siema",
-              "ELO",
-              "Elo",
-              "elo",
-              "UWAGA KRYCHA MROZI",
-              "Wyobraź sobie że żyjesz w XXI wieku"]
+client = commands.Bot(command_prefix = "~")
 
 #bot loggin
 @client.event
 async def on_ready():
     print("Logged in as {0.user}".format(client))
-
-#bot commands
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    
-    if message.content.startswith('HELP'):
-        await message.channel.send(help_list)
-
-    if message.content.startswith('WITAM'):
-        await message.channel.send(random.choice(witam_list))
 
 #web server
 keep_alive()
