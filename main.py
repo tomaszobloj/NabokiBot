2abc581eab1d6f60f1cea9617d1646e2e95c2b0c
Tomasz Obłój <61419484+tomaszobloj@users.noreply.github.com>
2021-09-19 10:07:07 +0000

    ping command, witam_list added

--- main.py
+++ main.py
@@ -4,27 +4,42 @@ import random
 from keep_alive import keep_alive
 from discord.ext import commands
 
-client = commands.Bot(command_prefix = "~")
+client = commands.Bot(command_prefix="~")
+
+witam_list = [
+    "WITAM!", "Witam!", "witam!", "WITAM", "Witam", "witam", "WITAM...",
+    "Witam...", "witam...", "Kij Ci w oko", "SIEMA", "Siema", "siema", "ELO",
+    "Elo", "elo", "UWAGA KRYCHA MROZI", "Wyobraź sobie że żyjesz w XXI wieku"
+]
+
 
 #bot loggin
 @client.event
 async def on_ready():
     print("Logged in as {0.user}".format(client))
 
+
 #member joined a server
 @client.event
 async def on_member_join(member):
-  print(f'{member} dołączył.')
+    print(f'{member} dołączył.')
+
 
 #member left a server
 @client.event
 async def on_member_remove(member):
-  print(f'{member} odszedł.')
+    print(f'{member} odszedł.')
+
 
 #bot commands
 @client.command()
 async def WITAM(ctx):
-  await ctx.send('WITAM')
+    await ctx.send(random.choice(witam_list))
+
+@client.command()
+async def PING(ctx):
+    await ctx.send(f'PONG! (round{client.latency * 1000}) ms')
+
 
 #web server
 keep_alive()
